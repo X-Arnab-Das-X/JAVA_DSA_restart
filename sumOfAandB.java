ac16a7370d0934e0aa1b961f6afc5837dffae3cc
Arnab Das <arnabdas@Arnabs-MacBook-Air-M2.local>
2026-05-14 18:23:10 +0530

    initial commit

---
+++ sumOfAandB.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sumOfAandB{
+    public static void main(String args[]){
+
+        int a = 5;
+        int b = 10;
+
+        b=a+b;
+
+        System.out.print(b);
+        
+    }
+}
